a7f052478be55606517002c9616f0aeddd429d2f
lesha <zamplt@ya.ru>
2024-05-08 16:54:49 +0300

    fuck me in the ass

--- src/brick_game/tetris/tetris.c
+++ src/brick_game/tetris/tetris.c
@@ -1,21 +1,25 @@
 #include "tetris.h"
 
 int drawFig(int figId, figure_t figure, int* fieldToPrint[HEIGHT][WIDTH]) {
-  for (int i = 0; i < 2; i++)
+  int* p = NULL;
+  for (int i = 0; i < 4; i++)
     for (int j = 0; j < 4; j++) {
       if (figure.rotation == 0)
-        (*fieldToPrint)[i + figure.y][j + figure.x] = figure.fig[i][j];
+        p = &((*fieldToPrint)[i + figure.y][j + figure.x]);
       else if (figure.rotation == 1)
-        (*fieldToPrint)[i + figure.y][j + figure.x] = figure.fig[i][j];
-      else if (figure.rotation == 1)
-        (*fieldToPrint)[i + figure.y][j + figure.x] = figure.fig[i][j];
-      else if (figure.rotation == 1)
-        (*fieldToPrint)[i + figure.y][j + figure.x] = figure.fig[i][j];
+        p = &((*fieldToPrint)[j + figure.y][3 - i + figure.x]);
+      else if (figure.rotation == 2)
+        p = &((*fieldToPrint)[3 - i + figure.y][3 - j + figure.x]);
+      else if (figure.rotation == 3)
+        p = &((*fieldToPrint)[3 - j + figure.y][i + figure.x]);
+      if (figure.fig[i][j] && *p) return 1;  // занято нахуй
     }
+  return 0;  // success
 }
 
 int main() {
-  struct Figure currentFigure = {0, {{0, 0, 0, 0}, {0, 0, 0, 0}}, 0, 3, 0};
+  struct Figure currentFigure = {
+      0, {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}, 0, 3, 0};
   int score = 0, level = 1, gameState = 0, time = 0, collision = 1;
   int nextFigureID = rand() % 7;
   int field[HEIGHT][WIDTH] = {0};
@@ -23,7 +27,7 @@ int main() {
   int figList[7][2][4] = {
       {{4, 4, 4, 4}, {0, 0, 0, 0}},  // I
       {{0, 2, 0, 0}, {0, 2, 2, 2}},  // J
-      {{0, 0, 0, 3}, {0, 3, 3, 3}},  // L
+      {{0, 0, 3, 0}, {3, 3, 3, 0}},  // L
       {{0, 7, 7, 0}, {0, 7, 7, 0}},  // O
       {{0, 5, 5, 0}, {0, 0, 5, 5}},  // S
       {{0, 0, 6, 0}, {0, 6, 6, 6}},  // T
@@ -36,6 +40,12 @@ int main() {
 
       currentFigure.y = collision = currentFigure.rotation = 0;
       currentFigure.figID = nextFigureID;
+
+      for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 4; j++) {
+          currentFigure.fig[i ? 0 : 3][j] = 0;  // кастыыыыль
+          currentFigure.fig[i + 1][j] = figList[currentFigure.figID][i][j];
+        }
       nextFigureID = rand() % 7;
     }
 
